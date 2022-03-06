97dcb49e869e300e37e5fa7af3b0385f281268c3
곰 세 마리가 한 집에 있어 <89408540+oengoeiyy@users.noreply.github.com>
2022-03-06 16:24:58 +0700

    Add files via upload

--- solidity.sol
+++ solidity.sol
@@ -42,17 +42,19 @@ contract b6210236_b6238124 {
   
 
     // record a reservation
-  function lagoon(string memory name,string memory reserver,string memory timestamp) public payable {
+  function poly(string memory name,string memory reserver,string memory timestamp) public payable {
     //require(msg.value == 0.001 ether);
-    if(keccak256(bytes(name)) != keccak256(bytes("lagoon"))) {
+    if(keccak256(bytes(name)) != keccak256(bytes("poly"))) {
         //---fire the event---
-        emit ReserveError(msg.sender, name, "Lagoon not found. Please contact admin.");
+        emit ReserveError(msg.sender, name, "Polynesian not found. Please contact admin.");
+        //---refund back to the sender---
+        payable(msg.sender).transfer(msg.value);
         //---exit the function---
         return;
     }
     else if (msg.value != 0.001 ether) {
         //---fire the event---
-        emit ReserveError(msg.sender, name, "Lagoon require 0.001 Ether. Please repay.");
+        emit ReserveError(msg.sender, name, "Polynesian require 0.001 ETH. Please repay.");
         //---refund back to the sender---
         payable(msg.sender).transfer(msg.value);
         //---exit the function---
@@ -63,14 +65,14 @@ contract b6210236_b6238124 {
     
   }
 
-  function ocean(string memory name,string memory reserver,string memory timestamp) public payable {
-    //require(msg.value == 0.002 ether);
-    if(keccak256(bytes(name)) != keccak256(bytes("ocean"))) {
-        emit ReserveError(msg.sender, name, "Ocean not found. Please contact admin.");
+  function space(string memory name,string memory reserver,string memory timestamp) public payable {
+    if(keccak256(bytes(name)) != keccak256(bytes("space"))) {
+        emit ReserveError(msg.sender, name, "Space not found. Please contact admin.");
+        payable(msg.sender).transfer(msg.value);
         return;
     }
     else if (msg.value != 0.002 ether) {
-        emit ReserveError(msg.sender, name, "Ocean require 0.002 Ether. Please repay.");
+        emit ReserveError(msg.sender, name, "Space require 0.002 ETH. Please repay.");
         payable(msg.sender).transfer(msg.value);
         return;
     }
@@ -79,14 +81,14 @@ contract b6210236_b6238124 {
     
   }
 
-  function beach(string memory name,string memory reserver,string memory timestamp) public payable {
-    //require(msg.value == 0.003 ether);
-    if(keccak256(bytes(name)) != keccak256(bytes("beach"))) {
-        emit ReserveError(msg.sender, name, "Beach not found. Please contact admin.");
+  function roman(string memory name,string memory reserver,string memory timestamp) public payable {
+    if(keccak256(bytes(name)) != keccak256(bytes("roman"))) {
+        emit ReserveError(msg.sender, name, "Roman not found. Please contact admin.");
+        payable(msg.sender).transfer(msg.value);
         return;
     }
     else if (msg.value != 0.003 ether) {
-        emit ReserveError(msg.sender, name, "Beach require 0.003 Ether. Please repay.");
+        emit ReserveError(msg.sender, name, "Roman require 0.003 ETH. Please repay.");
         payable(msg.sender).transfer(msg.value);
         return;
     }
@@ -95,14 +97,14 @@ contract b6210236_b6238124 {
     
   }
 
-  function pool(string memory name,string memory reserver,string memory timestamp) public payable {
-    //require(msg.value == 0.004 ether);
-    if(keccak256(bytes(name)) != keccak256(bytes("pool"))) {
-        emit ReserveError(msg.sender, name, "Pool not found. Please contact admin.");
+  function vict(string memory name,string memory reserver,string memory timestamp) public payable {
+    if(keccak256(bytes(name)) != keccak256(bytes("vict"))) {
+        emit ReserveError(msg.sender, name, "Victorain Coach not found. Please contact admin.");
+        payable(msg.sender).transfer(msg.value);
         return;
     }
     else if (msg.value != 0.004 ether) {
-        emit ReserveError(msg.sender, name, "Pool require 0.003 Ether. Please repay.");
+        emit ReserveError(msg.sender, name, "Victorain Coach require 0.004 ETH. Please repay.");
         payable(msg.sender).transfer(msg.value);
         return;
     }
@@ -110,14 +112,14 @@ contract b6210236_b6238124 {
     emit ReserveAdd(msg.sender, name,hashing(name),reserver,timestamp);
     
   }
-  function reef(string memory name,string memory reserver,string memory timestamp) public payable {
-    //require(msg.value == 0.005 ether);
-    if(keccak256(bytes(name)) != keccak256(bytes("reef"))) {
-        emit ReserveError(msg.sender, name, "Reef not found. Please contact admin.");
+  function truck(string memory name,string memory reserver,string memory timestamp) public payable {
+    if(keccak256(bytes(name)) != keccak256(bytes("truck"))) {
+        emit ReserveError(msg.sender, name, "Luxury Truck not found. Please contact admin.");
+        payable(msg.sender).transfer(msg.value);
         return;
     }
-    else if (msg.value != 0.004 ether) {
-        emit ReserveError(msg.sender, name, "Reef require 0.005 Ether. Please repay.");
+    else if (msg.value != 0.005 ether) {
+        emit ReserveError(msg.sender, name, "Luxury Truck require 0.005 ETH. Please repay.");
         payable(msg.sender).transfer(msg.value);
         return;
     }
@@ -126,14 +128,14 @@ contract b6210236_b6238124 {
     
   }
 
-  function grand(string memory name,string memory reserver,string memory timestamp) public payable {
-    //require(msg.value == 0.006 ether);
-    if(keccak256(bytes(name)) != keccak256(bytes("grand"))) {
-        emit ReserveError(msg.sender, name, "Grand not found. Please contact admin.");
+  function prince(string memory name,string memory reserver,string memory timestamp) public payable {
+    if(keccak256(bytes(name)) != keccak256(bytes("prince"))) {
+        emit ReserveError(msg.sender, name, "Luxury Princess Room not found. Please contact admin.");
+        payable(msg.sender).transfer(msg.value);
         return;
     }
     else if (msg.value != 0.006 ether) {
-        emit ReserveError(msg.sender, name, "Grand require 0.005 Ether. Please repay.");
+        emit ReserveError(msg.sender, name, "Luxury Princess Room require 0.006 ETH. Please repay.");
         payable(msg.sender).transfer(msg.value);
         return;
     }
@@ -149,8 +151,8 @@ contract b6210236_b6238124 {
     return sha256(bytes(name));
   }
   
-  // check name of student in this class
-  function checkName(string memory name) public 
+  // check room
+  function checkRoom(string memory name) public 
   view returns (bool) {
     return listRoom[hashing(name)];
   }
